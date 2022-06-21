76b1fcd81f050578d6d8d73fc1baa38bc121949e
Robert2510 <contact.rguzman@gmail.com>
2022-06-21 00:09:07 -0300

    decuentos

---
+++ descuentos.js
@@ -0,0 +1,23 @@
+
+//const precioOriginal = 230;
+//const descuento = 17;
+//const porcentajeConDescuento = 100 - descuento;
+//const precioFinal = (precioOriginal * porcentajeConDescuento)/100 ;
+
+/*console.log({
+    precioOriginal,
+    descuento,
+    porcentajeConDescuento,
+    precioFinal
+});
+*/
+
+const descuento= 15;
+
+function calcularPrecioFinal(precio,decuento){
+    const porcentajeConDescuento = 100 - descuento;
+    const precioFinal = (precio * porcentajeConDescuento)/100;
+
+
+
+}
