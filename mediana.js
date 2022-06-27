a43b5b442dd9cf384c6a60bb7af7657326fea38e
Robert2510 <contact.rguzman@gmail.com>
2022-06-26 23:49:41 -0300

    practica de mediana

--- mediana.js
+++ mediana.js
@@ -1,11 +1,58 @@
+const number = [
+    100,
+    200,
+    2100,
+    40000,    
+]
 
-
-var number= [4,2,5,1,3];
 number.sort(function(a,b){
     return a-b;
     
 })
+    
+  console.log(number);
 
-const numberOrdenados= number.sort();
+  mitadLista = parseInt(number.length / 2);
+  //este es la mediana si el total de elementos es impar//
 
-console.log(number);
+
+  function calcularPromedio(lista){
+     let sumaLista = 0;
+ 
+    for (let i= 0; i < lista.length; i++){
+     sumaLista= sumaLista+lista[i];
+ 
+ }
+}
+ 
+
+
+  function esPar(numero){
+    if (numero % 2 === 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+  }
+
+  let mediana;
+
+  if (esPar(number.length)){
+    const elemento1= number[mitadLista -1];
+    const elemento2= number[mitadLista];
+
+    const promedio = calcularPromedio([
+        elemento1,
+        elemento2,
+    ])
+
+    mediana = promedio;
+  }
+  else {
+    mediana = number[number];
+  }
+
+
+  
+
