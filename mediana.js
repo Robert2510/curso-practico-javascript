cb4246762f7b02045a2e6ba9ca71f29eb82945fe
Robert2510 <contact.rguzman@gmail.com>
2022-07-03 23:22:07 -0300

    moda

--- mediana.js
+++ mediana.js
@@ -1,58 +1,69 @@
+function calcularPromedio(number){
+    let sumaNumber = 0;
+    for (let i= 0; i < number.length; i++){
+        sumaNumber= sumaNumber+number[i];
+
+        const promedioNumber = sumaNumber/ number.length;
+   
+        return promedioNumber;
+    }
+
+}
+
 const number = [
+    
+    300,
     100,
-    200,
-    2100,
-    40000,    
-]
+    40000,
+    2100,    
+];
+
+ 
 
 number.sort(function(a,b){
     return a-b;
     
-})
+});
     
   console.log(number);
 
-  mitadLista = parseInt(number.length / 2);
+  mitadNumber =parseInt(number.length / 2);
   //este es la mediana si el total de elementos es impar//
 
 
-  function calcularPromedio(lista){
-     let sumaLista = 0;
- 
-    for (let i= 0; i < lista.length; i++){
-     sumaLista= sumaLista+lista[i];
- 
- }
-}
- 
-
-
-  function esPar(numero){
-    if (numero % 2 === 0){
+ function esPar(number){
+    if (number % 2 === 0){
         return true;
     }
     else{
         return false;
     }
-  }
-
-  let mediana;
-
-  if (esPar(number.length)){
-    const elemento1= number[mitadLista -1];
-    const elemento2= number[mitadLista];
-
-    const promedio = calcularPromedio([
-        elemento1,
-        elemento2,
-    ])
-
-    mediana = promedio;
-  }
-  else {
-    mediana = number[number];
-  }
+}
 
 
   
+
+    let mediana;
+
+if (number=== true){
+    const elemento1=number[mitadNumber-1];
+    const elemento2=number[mitadNumber];
+    let mediana =(elemento1+elemento2) /2;
+    
+}else{
+    mediana= number[mitadNumber];
+}
+
+/*if (esPar(number.length)){
+    const elemento1=number[mitadNumber-1];
+    const elemento2=number[mitadNumber];
+
+    const promedioElemento1Y2= calcularPromedio([elemento1,elemento2]);
+
+    mediana=promedioElemento1Y2;
+
+}else{
+    mediana= number[mitadLista];
+}*/
+ 
 
